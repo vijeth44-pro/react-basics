1fa06bcd9a75b67a3d23d8d25a0fd020f0f9ece6
vijeth44-pro <captainmncaptain@gmail.com>
2026-01-13 08:28:53 +0530

    feat: update title in index.html and implement Navbar component with styling

--- webpage/src/components/navbar.jsx
+++ webpage/src/components/navbar.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./navbar.css";
 
 export default function navbar() {
   return (
-    <div>
-        <div>
-            
-        </div>
+    <div className="container">
+      <div className="cont-components">
+        <h1>meesho</h1>
+        <input
+          type="search"
+          placeholder="Try Saree,Kurti or Search by product Code"
+        ></input>
+        <h1>Become a supplier</h1>
+        <h1>Investor Relations</h1>
+      </div>
     </div>
-  )
+  );
 }
