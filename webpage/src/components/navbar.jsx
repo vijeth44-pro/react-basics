7190142371e8253ce78303187c2182d9da81e3dc
vijeth44-pro <captainmncaptain@gmail.com>
2026-01-13 09:12:01 +0530

    refactor: clean up Navbar component by removing unused elements and update body background color in CSS

--- webpage/src/components/navbar.jsx
+++ webpage/src/components/navbar.jsx
@@ -3,16 +3,8 @@ import "./navbar.css";
 
 export default function navbar() {
   return (
-    <div className="container">
-      <div className="cont-components">
-        <h1>meesho</h1>
-        <input
-          type="search"
-          placeholder="Try Saree,Kurti or Search by product Code"
-        ></input>
-        <h1>Become a supplier</h1>
-        <h1>Investor Relations</h1>
-      </div>
+    <div className="navbar">
+      
     </div>
   );
 }
