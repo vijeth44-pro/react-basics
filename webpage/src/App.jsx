a7c13e981aba1333adbfedf12341f77d78b77d93
vijeth44-pro <captainmncaptain@gmail.com>
2026-01-14 17:01:36 +0530

    Add banner component to display promotional image

--- webpage/src/App.jsx
+++ webpage/src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/navbar.jsx'
+import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import Banner from'./components/banner.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div>
-      <div>
-        <Navbar />
-      </div>
+    <div className="container">
+      <Navbar />
+      <Banner />
     </div>
   )
 }
---
+++ webpage/src/components/banner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function banner() {
+  return (
+    <div>
+        <img src="../assests/banner.jpg" alt="banner" />
+    </div>
+  )
+}
