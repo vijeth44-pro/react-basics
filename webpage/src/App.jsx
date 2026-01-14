0dcfa03754e61c48d8c8eed92a31e74fc86e7c36
vijeth44-pro <captainmncaptain@gmail.com>
2026-01-14 19:52:51 +0530

    feat: add Card component to display product information; update App component to include Card instances; modify Navbar styles

--- webpage/src/App.jsx
+++ webpage/src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from './components/navbar.jsx'
 import Banner from './components/banner.jsx'
+import Card from './components/card.jsx'
 
 const App = () => {
   return (
     <div className="container">
       <Navbar />
       <Banner />
+      <div style={{display:'flex', gap:'20px', padding:'20px'}}>
+        <Card title={"Watch"} description={"Stylish analog watch for men and boys in chain"} image={'https://images.meesho.com/images/products/400056869/ne0yw_512.avif?width=360'} />
+        <Card title={"Shoe"} description={"Modern Trendy Men Casual Shoes"} image={'https://images.meesho.com/images/products/434491245/jmvmi_512.avif?width=360'}/>
+        <Card title={"Flower Pots [Plastic]"} description={"5 PIS COMBO OF THE Plastic Round Flower Pots for Home Planters, Terrace, Garden Etc | Pack of 05 | Multicolor | Suitable for Home Indoor & Outdoor Gardening Plants"} image={'https://images.meesho.com/images/products/422365361/lr4v0_512.avif?width=512'}/>
+        <Card title={"Oppo"} description={"OPPO A77s 8GB 128GB Starry Black- 4G Smartphone"} image={'https://images.meesho.com/images/products/540547590/kbjmx_512.avif?width=512'}/>
+        <Card title={"Ghar Soap"} description={"GHAR SOAPS MAGIC SOAP 100 GM"} image={'https://images.meesho.com/images/products/328554683/rrzge_512.avif?width=512'}/>
+      </div>
     </div>
   )
 }
---
+++ webpage/src/components/card.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function card({title,description,image}) {
+    const newStyle={
+        backgroundColor:'lightgrey',
+        width:'300px',
+        height:'450px',
+        color:'black',
+        borderRadius:'10px'
+    }
+  return (
+    <div>
+        <div style={newStyle}>
+            <img src={image} alt="cards" style={{width:'300px', height:'300px' , borderRadius:'10px'}}/>
+            <h1 style={{textAlign:'center'}}>{title}</h1>
+            <p style={{textAlign:'center', fontSize:'15px'}}>{description}</p>
+        </div>
+    </div>
+  )
+}
